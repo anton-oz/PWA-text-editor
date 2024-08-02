af8d2881bf8103c9a08f6767a939c1981f9ed826
anton <antonosland789@gmail.com>
2024-08-02 09:43:46 -0500

    app caching properly, no errors in console, working on install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log('PUT to db');
+    const jateDB = await openDB('jate', 1);
+    const tx = jateDB.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ content });
+    const result = await request;
+    console.log('saved to db', result);
+  }
+  catch (err)  {
+    console.error(err);
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GET all from db');
+    const jateDB = await openDB('jate', 1);
+    const tx = jateDB.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('db result', result);
+  }
+  catch (err) {
+    console.error(err);
+  }
+};
+
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,60 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      hot: 'only',
+      // contentBase: path.resolve(__dirname, 'dist'),
+    },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE text editor',
+        short_name: 'JATE',
+        description: 'Browser text editor that you can install and use offline!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.png$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
